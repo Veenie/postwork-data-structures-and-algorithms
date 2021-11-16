c9a5cb560b1f1bf41ebccd34be559b07775079ef
Veenie <raveen08@gmail.com>
2021-11-15 17:42:05 -0800

    fibonacci solution

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/ruby/fibonacci.rb
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/ruby/fibonacci.rb
@@ -1,5 +1,10 @@
 def fibonacci(num)
   # type your code in here
+  if (num <= 1)
+    return num
+  else 
+    return fibonacci(num -1) + fibonacci(num -2)
+  end   
 end
 
 if __FILE__ == $PROGRAM_NAME
